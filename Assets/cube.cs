a6ba93302c1a2304faca8d6e8e18221c973f2f8d
x0915583317 <x0915583317@gmail.com>
2016-07-27 16:23:00 +0800

    0

---
+++ Assets/cube.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class cube : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float ver = Input.GetAxis("Vertical");
+		float hor = Input.GetAxis("Horizontal");
+		Vector2 vec = new Vector2(ver,hor);
+		if(vec.magnitude > 0)
+		{
+			transform.position = new Vector3(transform.position.y, transform.position.z+hor);
+		}
+	}
+}
